ad1c97a0c04e5148d7e3dd2f2a1ddce268c631f8
Ivan Arregoitia <ivanarrego@gmail.com>
2019-05-21 18:57:34 -0700

    BE-02 | Ivan Arregoitia R3

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -2,13 +2,8 @@
   require_once("functions.php");
   require_once("db_connection.php");
   set_exception_handler("error_handler");
-  print_r($conn);
-
-  $query = "SELECT * FROM `wicked-sales`";
-  $result = mysqli_query($conn, $query);
-
-  if (!$result){
-      print ("error" . mysqli_error($conn));
+  if (!$conn){
+    throw new Exception(mysqli_connect_error($conn));
       exit();
   };
 ?>
