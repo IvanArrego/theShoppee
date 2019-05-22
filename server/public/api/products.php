2a5c1f816df6d89e4ff3bd369a08338e0eb1c338
Ivan Arregoitia <ivanarrego@gmail.com>
2019-05-21 17:53:56 -0700

    BE-02-Revised | Ivan Arregoitia

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -2,8 +2,7 @@
   require_once("functions.php");
   require_once("db_connection.php");
   set_exception_handler("error_handler");
-  $output = file_get_contents('dummy-products-list.json');
-  print($output);
+  print_r($conn);
 
   $query = "SELECT * FROM `wicked-sales`";
   $result = mysqli_query($conn, $query);
