ca3b422ef8617a291c504e0ae5d2e04a13faa7c1
Ivan Arregoitia <ivanarrego@gmail.com>
2019-09-24 20:06:02 -0700

    Added a confirmation modal when user is in cart and would like to delete the item, fixed the casing of the cart buttons so that they are now uniformed, and fixed an issue where if the cart was empty there was tax.

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import { Col, Container, Row, Modal, ModalBody, ModalFooter, ModalHeader, Button } from 'reactstrap';
 import ReactTooltip from 'react-tooltip';
 
 class CartSummaryItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modal: false
+    };
     this.deleteItem = this.deleteItem.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
   deleteItem() {
     this.props.onDelete(this.props.id);
+  }
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
   }
   render() {
     const realPrice = parseFloat(this.props.price / 100);
@@ -21,9 +30,19 @@ class CartSummaryItem extends React.Component {
             <div className="h3 card-font">{this.props.name}</div>
             <div className="h6">{this.props.desc}</div>
             <div className="h6 description-font text-muted">${realPrice.toFixed(2)}</div>
-            <button className="btn btn-lg btn-block card-font btn-danger" onClick={this.deleteItem}>Delete</button>
+            <button className="btn btn-lg btn-block card-font btn-danger" onClick={this.toggle}>Delete</button>
           </div>
         </div>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader className="card-font" toggle={this.toggle}><i className="fas fa-exclamation-circle text-warning"></i> FINAL CONFIRMATION</ModalHeader>
+          <ModalBody>
+            <div className="container mb-3 text-center description-font font-weight-bold">Are you sure you would like to remove this from cart?</div>
+          </ModalBody>
+          <ModalFooter className="card-font">
+            <Button color="secondary" onClick={this.toggle}>No</Button>{' '}
+            <Button color="success" onClick={this.deleteItem}>Yes</Button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
@@ -84,7 +103,7 @@ function CartSummary(props) {
             </ReactTooltip>
             <hr/>
             <div className="h4 card-font mb-4">TOTAL : <span className="float-right">${totalTaxedPrice}</span></div>
-            <button type="button" className="btn btn-lg btn-secondary btn-block card-font" onClick={() => props.setView('catalog', { })}>BACK TO CATALOG</button>
+            <button type="button" className="btn btn-lg btn-secondary btn-block card-font" onClick={() => props.setView('catalog', { })}>Back To Catalog</button>
             <button type="button" className="btn btn-lg btn-primary btn-block card-font" onClick={() => props.setView('checkout', { })}>Checkout</button>
           </Col>
         </Row>
@@ -106,13 +125,13 @@ function CartSummary(props) {
             <ReactTooltip id="tooltip-cart1" place="right" type="dark" effect="solid">
               <span className="font-weight-bold">Shipping is set at a flat-rate of $5</span>
             </ReactTooltip>
-            <div className="h6 description-font mb-4">Tax: <i className="fas fa-question-circle pointer-hover text-warning" href="#" data-tip data-for="tooltip-cart2"/> <span className="float-right">$5</span></div>
+            <div className="h6 description-font mb-4">Tax: <i className="fas fa-question-circle pointer-hover text-warning" href="#" data-tip data-for="tooltip-cart2"/> <span className="float-right">$0.00</span></div>
             <ReactTooltip id="tooltip-cart2" place="right" type="dark" effect="solid">
               <span className="font-weight-bold">Sales tax is based on Orange County, CA&apos;s rate of 7.75%</span>
             </ReactTooltip>
             <hr/>
             <div className="h4 card-font mb-4">TOTAL : <span className="float-right">$0.00</span></div>
-            <button type="button" className="btn btn-lg btn-secondary btn-block card-font" onClick={() => props.setView('catalog', { })}>BACK TO CATALOG</button>
+            <button type="button" className="btn btn-lg btn-secondary btn-block card-font" onClick={() => props.setView('catalog', { })}>Back To Catalog</button>
           </Col>
         </Row>
       </Container>
